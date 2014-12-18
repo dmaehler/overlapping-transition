94a9f2d3d6dc99ceaa0214c46e5a7db70d365ffc
Diego Maehler <dmaehler@gmail.com>
2014-12-18 16:12:13 -0200

    Finished the push and pop views' transitions.

--- overlapping-transition/OverlappingPopAnimation.m
+++ overlapping-transition/OverlappingPopAnimation.m
@@ -8,6 +8,8 @@
 
 #import "OverlappingPopAnimation.h"
 
+#define DEGREES_TO_RADIANS(angle) ((angle) / 180.0 * M_PI)
+
 @implementation OverlappingPopAnimation
 
 - (NSTimeInterval)transitionDuration:(id <UIViewControllerContextTransitioning>)transitionContext
@@ -17,6 +19,37 @@
 
 - (void)animateTransition:(id <UIViewControllerContextTransitioning>)transitionContext
 {
+    UIViewController *fromVC = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
+    UIViewController *toVC = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
+    
+    [[transitionContext containerView] addSubview:toVC.view];
+    
+    [UIView animateKeyframesWithDuration:[self transitionDuration:transitionContext]
+                                   delay:0.0
+                                 options:UIViewKeyframeAnimationOptionLayoutSubviews
+                              animations:^{
+                                  //Animate the current view to the behind scene rotating it 'till lay it down
+                                  [UIView addKeyframeWithRelativeStartTime:0.0
+                                                          relativeDuration:0.6
+                                                                animations:^{
+                                                                    CATransform3D t = CATransform3DIdentity;
+                                                                    t.m34 = 1.0/500;
+                                                                    t = CATransform3DRotate(t,DEGREES_TO_RADIANS(-90), 1, 0, 0);
+                                                                    fromVC.view.layer.transform = t;
+                                                                }];
+                                  //Animate back downward the up view
+                                  [UIView addKeyframeWithRelativeStartTime:0.4
+                                                          relativeDuration:0.6
+                                                                animations:^{
+                                                                    CATransform3D t = CATransform3DIdentity;
+                                                                    t.m34 = 1.0/500;
+                                                                    t = CATransform3DRotate(t, DEGREES_TO_RADIANS(0), 1, 0, 0);
+                                                                    toVC.view.layer.transform = t;
+                                                                }];
+                              }
+                              completion:^(BOOL finished) {
+                                  [transitionContext completeTransition:YES];
+                              }];
     
 }
 
--- overlapping-transition/OverlappingPushAnimation.m
+++ overlapping-transition/OverlappingPushAnimation.m
@@ -14,7 +14,7 @@
 
 - (NSTimeInterval)transitionDuration:(id <UIViewControllerContextTransitioning>)transitionContext
 {
-    return 2.0f;
+    return 1.0f;
 }
 
 - (void)animateTransition:(id <UIViewControllerContextTransitioning>)transitionContext
@@ -22,37 +22,42 @@
     UIViewController *fromVC = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
     UIViewController *toVC = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
     
+    //Get frames for the two Views
     CGRect fromFrame = [transitionContext initialFrameForViewController:fromVC];
     CGRect toFrame = [transitionContext finalFrameForViewController:toVC];
     
+    //Set anchor points to animate the toVC from bottom and fromVC from top
     fromVC.view.layer.anchorPoint = CGPointMake(0.5, 0);
     toVC.view.layer.anchorPoint = CGPointMake(0.5, 1);
     
+    //Sets back the frames for correctly the anchor points
     fromVC.view.frame = fromFrame;
     toVC.view.frame = toFrame;
     
+    //Start the toVC's view from laying down behind the scene
     CATransform3D t = CATransform3DIdentity;
     t.m34 = 1.0/500;
-    t = CATransform3DRotate(t, DEGREES_TO_RADIANS(-60), 1, 0, 0);
+    t = CATransform3DRotate(t, DEGREES_TO_RADIANS(-90), 1, 0, 0);
     toVC.view.layer.transform = t;
 
     [[transitionContext containerView] addSubview:toVC.view];
-    [[transitionContext containerView] sendSubviewToBack:toVC.view];
     
     [UIView animateKeyframesWithDuration:[self transitionDuration:transitionContext]
                                    delay:0.0
                                  options:UIViewKeyframeAnimationOptionLayoutSubviews
                               animations:^{
+                                  //Animate the fromVC rotating it to up backward
                                   [UIView addKeyframeWithRelativeStartTime:0.0
-                                                          relativeDuration:0.4
+                                                          relativeDuration:0.6
                                                                 animations:^{
                                                                     CATransform3D t = CATransform3DIdentity;
                                                                     t.m34 = 1.0/500;
-                                                                    t = CATransform3DRotate(t, DEGREES_TO_RADIANS(60), 1, 0, 0);
+                                                                    t = CATransform3DRotate(t, DEGREES_TO_RADIANS(90), 1, 0, 0);
                                                                     fromVC.view.layer.transform = t;
                                                                 }];
-                                  [UIView addKeyframeWithRelativeStartTime:0.5
-                                                          relativeDuration:0.4
+                                  //Animate the toVC from down to the front of scene
+                                  [UIView addKeyframeWithRelativeStartTime:0.4
+                                                          relativeDuration:0.6
                                                                 animations:^{
                                                                     CATransform3D t = CATransform3DIdentity;
                                                                     t.m34 = 1.0/500;
